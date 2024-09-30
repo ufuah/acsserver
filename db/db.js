651ca6d3dbac2eec7fa3ec0231017777f0ea4798
scott <ufuahemi@gmail.com>
2024-09-30 20:35:30 +0100

    db updat

--- db/db.js
+++ db/db.js
@@ -1,15 +1,15 @@
-import mysql from 'mysql';
+import mysql from "mysql";
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'user',
-  password: '123456789',
-  database: 'aceglobal'
+  host: "p4w8cckgwscok0c0coccowsk",
+  user: "mysql",
+  password: "nK0BEMPe7UBe6auXbR7ycPqS4X0c46sPMlIGAcVB2mmAc99gXZ3UqfG23V20ai21",
+  database: "aceglobal",
 });
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log('Connected to MySQL database');
+  console.log("Connected to MySQL database");
 });
 
 export default connection;
